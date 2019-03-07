99184739dbaa941e8874ad3255d63fcbf1ae3134
Ashley Owens <ash4741@gmail.com>
2019-03-06 22:59:16 -0500

    Update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server.js');
+require('dotenv').config();
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const server = require('./api/server.js');
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on PORT ${PORT} ***\n`);
 });
